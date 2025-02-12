38fe007c6cccf0e16212e8526535b0fa541676bc
kamoSantana <kamosantana100@gmail.com>
2025-02-12 14:06:20 +0200

    Yes or NO

---
+++ script (1).js
@@ -0,0 +1,9 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('Yay! I am so happy you said yes!');
+    // You can add more actions here, like redirecting to another page or showing an additional message.
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Oh no! Maybe next time.');
+    // You can add more actions here, like redirecting to another page or showing an additional message.
+});
